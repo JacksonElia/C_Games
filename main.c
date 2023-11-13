e01ab126a2e2c2c8c1a5c7e9972c5518454a8949
Jack <85963782+JacksonElia@users.noreply.github.com>
2023-11-12 23:35:30 -0500

    Added code to hide cursor

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
+#include <stdio.h>
+#include <windows.h>
 #include "snake.h"
 
 int main() {
+    // Hides cursor
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO info;
+    info.dwSize = 100;
+    info.bVisible = FALSE;
+    SetConsoleCursorInfo(consoleHandle, &info);
 
     snake_setup(50, 30, 10);
 
