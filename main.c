b58c3596b43188783b0d8c10da07730acfce79ad
Jack <85963782+JacksonElia@users.noreply.github.com>
2023-11-17 22:58:29 -0500

    Getting size of window

--- main.c
+++ main.c
@@ -2,14 +2,26 @@
 #include <windows.h>
 #include "drawingSetup.h"
 #include "snake.h"
+#include <unistd.h>
 
 
 int main() {
     hideCursor();
 
-    snake_setup(50, 30, 10);
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int columns, rows;
 
-    snake_main_loop();
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+
+    printf("columns: %d\n", columns);
+    printf("rows: %d\n", rows);
+
+
+//    snake_setup(50, 30, 10);
+//
+//    snake_main_loop();
 
     return 0;
 }
