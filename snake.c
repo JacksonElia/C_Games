b6055c81d05ac84cee2115770deae008d776a309
Jack <85963782+JacksonElia@users.noreply.github.com>
2023-12-27 16:41:31 -0500

    Getting closer to finishing buffer

--- snake.c
+++ snake.c
@@ -20,7 +20,7 @@ void draw() {
 //    system("cls");
     for (int i = 0; i < height; i++) {
         for (int j = 0; j < width; j++) {
-            if (i == 0 || i == width - 1 || j == 0 || j == height - 1) {
+            if (i == 0 || i == height - 1 || j == 0 || j == width - 1) {
                 printf("#");
             } else {
                 printf(" ");
